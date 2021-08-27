7d229ae874f6d0c5123a411ff5252f76ae0cf79b
Pascal Gillet <pascal.gillet@stack-labs.com>
2021-08-27 19:29:43 +0200

    Complete 0-Extend-a-JVM-based-application/

--- 0-Extend-a-JVM-based-application/run.sh
+++ 0-Extend-a-JVM-based-application/run.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 source ../utils/bash-env.sh
 
-cecho Gs "Compile"
-$GRAALVM_CE_HOME/bin/javac Validator.java
-
-cecho Gs "Install validator.js"
-$GRAALVM_CE_HOME/bin/npm install validator
-
-cecho Gs "Test"
+cecho Gs "Test Validator \U1F91E"
 $GRAALVM_CE_HOME/bin/java Validator isEmail pascal.gillet@stack-labs.com
 $GRAALVM_CE_HOME/bin/java Validator isEmail pascal-dot-gillet-at-stack-labs-dot-com
+$GRAALVM_CE_HOME/bin/java Validator isIP 127.0.0.1
+$GRAALVM_CE_HOME/bin/java Validator isIP 127.0.0.999
