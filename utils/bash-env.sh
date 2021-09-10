e2191e4587169dfa57131b0da5acd22873ecc143
Pascal Gillet <pascal.gillet@stack-labs.com>
2021-09-10 17:28:57 +0200

    Display user prompt in dark gray

--- utils/bash-env.sh
+++ utils/bash-env.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR="$(dirname ${BASH_SOURCE[0]})"
-
 source ${SCRIPT_DIR}/bash-colors/bash-colors.sh
 
 set -eu
 
+function process_command() {
 # Print commands except echo commands
-PROMPT="Press any key to continue"
+if [[ $BASH_COMMAND != cecho* ]]; then
+    echo $BASH_COMMAND
+    read -n 1 -p $'\e[90mPress any key to continue\e[0m'
+fi
+}
 
-trap '[[ $BASH_COMMAND != cecho* ]] && echo $BASH_COMMAND && read -n 1 -p "$PROMPT"' DEBUG
+trap process_command DEBUG
 
