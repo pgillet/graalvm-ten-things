dabf2c4ae6f85c721aeff4953bf5a661381ea244
Pascal Gillet <pascal.gillet@stack-labs.com>
2021-09-13 16:05:26 +0200

    Improve bash env

--- 01-High-performance-modern-Java/run.sh
+++ 01-High-performance-modern-Java/run.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 source ../utils/bash-env.sh
 
-cecho Gs "Generate large text file"
+echo "Generate large text file"
 make large.txt
 
-cecho Gs "\nCompile"
+echo "Compile"
 $GRAALVM_EE_HOME/bin/javac TopTen.java
 
-cecho Gs "\nRun with GraalVM EE \U1F680"
+echo "Run with GraalVM EE \U1F680"
 time $GRAALVM_EE_HOME/bin/java TopTen large.txt
 
-# cecho "\nRun with GraalVM without the GraalVM JIT compiler"
+# echo "Run with GraalVM without the GraalVM JIT compiler"
 # time $GRAALVM_EE_HOME/bin/java -XX:-UseJVMCICompiler TopTen large.txt
 
-cecho Gs "\nRun with standard HotSpot JVM \U1F40C"
+echo "Run with standard HotSpot JVM \U1F40C"
 time $JAVA_HOME/bin/java TopTen large.txt
--- utils/bash-env.sh
+++ utils/bash-env.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR="$(dirname ${BASH_SOURCE[0]})"
-source ${SCRIPT_DIR}/bash-colors/bash-colors.sh
+# source ${SCRIPT_DIR}/bash-spinner/spinner.sh
 
 set -eu
 
+function echo() {
+    command echo -e "\n\e[3m\e[32m# $1\e[0m"
+}
+
 function process_command() {
 # Print commands except echo commands
-if [[ $BASH_COMMAND != cecho* ]]; then
-    echo $BASH_COMMAND
+if [[ $BASH_COMMAND != echo* ]]; then
+    printf "\e[1m%s\e[0m\n" "$BASH_COMMAND"
     read -n 1 -p $'\e[90mPress any key to continue\e[0m'
 fi
+#    start_spinner ""
 }
 
 trap process_command DEBUG
