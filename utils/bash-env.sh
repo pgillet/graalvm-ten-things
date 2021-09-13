c33103e81570ab072c5af0016d0d8aac9cf587ff
Pascal Gillet <pascal.gillet@stack-labs.com>
2021-09-13 16:48:51 +0200

    Read enter

--- utils/bash-env.sh
+++ utils/bash-env.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR="$(dirname ${BASH_SOURCE[0]})"
-# source ${SCRIPT_DIR}/bash-spinner/spinner.sh
 
 set -eu
 
@@ -13,9 +12,8 @@ function process_command() {
 # Print commands except echo commands
 if [[ $BASH_COMMAND != echo* ]]; then
     printf "\e[1m%s\e[0m\n" "$BASH_COMMAND"
-    read -n 1 -p $'\e[90mPress any key to continue\e[0m'
+    read -p $'\e[90mPress enter to continue\e[0m'
 fi
-#    start_spinner ""
 }
 
 trap process_command DEBUG
