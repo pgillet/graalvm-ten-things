ce2d8656d1dd844601073ab2fa1f1abf480eb453
Pascal Gillet <pascal.gillet@stack-labs.com>
2021-09-13 20:26:35 +0200

    Add native language example

---
+++ 0-Run-native-languages-on-the-JVM/run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+source ../utils/bash-env.sh
+
+echo "Compile using standard clang (the LLVM C compiler) \U1F916"
+clang --version
+
+clang -c -emit-llvm oggenc.c
+
+echo "Compilation result \U1F91E"
+stat -c "%s %n" *.bc
+
+echo "Run this directly using GraalVM using the lli command (LLVM bitcode interpreter)"
+$GRAALVM_CE_HOME/bin/lli --experimental-options --llvm.verifyBitcode=false oggenc.bc jfk_1963_0626_berliner.wav
+
+echo "Encoding result"
+stat -c "%s %n" *.wav *.ogg
+
+echo "Play the result \U1F3B5"
+# apt install vorbis-tools
+ffplay jfk_1963_0626_berliner.ogg
