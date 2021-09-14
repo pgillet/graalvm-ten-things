b0ff797713d505942a521cf7cf2e1802f574bfdf
Pascal Gillet <pascal.gillet@stack-labs.com>
2021-09-14 09:52:54 +0200

    Change wav sample

--- 0-Run-native-languages-on-the-JVM/run.sh
+++ 0-Run-native-languages-on-the-JVM/run.sh
@@ -10,11 +10,11 @@ echo "Compilation result \U1F91E"
 stat -c "%s %n" *.bc
 
 echo "Run this directly using GraalVM using the lli command (LLVM bitcode interpreter)"
-$GRAALVM_CE_HOME/bin/lli --experimental-options --llvm.verifyBitcode=false oggenc.bc jfk_1963_0626_berliner.wav
+$GRAALVM_CE_HOME/bin/lli --experimental-options --llvm.verifyBitcode=false oggenc.bc 62284main_onesmall2.wav
 
 echo "Encoding result"
 stat -c "%s %n" *.wav *.ogg
 
 echo "Play the result \U1F3B5"
 # apt install vorbis-tools
-ffplay jfk_1963_0626_berliner.ogg
+ffplay 62284main_onesmall2.ogg
