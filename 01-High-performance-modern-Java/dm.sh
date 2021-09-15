4e3c5061386eed97e5c2a74b09f7f42e7a77f5d5
Pascal Gillet <pascal.gillet@stack-labs.com>
2021-09-15 11:47:31 +0200

    Add Demo Magic sample

---
+++ 01-High-performance-modern-Java/dm.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+. ../utils/demo-magic/demo-magic.sh
+
+# hide the evidence
+clear
+
+ # Generate large text file
+pe "make large.txt"
+
+# Compile
+pe "$GRAALVM_EE_HOME/bin/javac TopTen.java"
+
+# Run with GraalVM EE
+pe "time $GRAALVM_EE_HOME/bin/java TopTen large.txt"
+
+# Run with GraalVM without the GraalVM JIT compiler
+# pe "time $GRAALVM_EE_HOME/bin/java -XX:-UseJVMCICompiler TopTen large.txt"
+
+# Run with standard HotSpot JVM
+pe "time $JAVA_HOME/bin/java TopTen large.txt"
