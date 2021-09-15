87a8f3eb38de4ca04e4478db25791eadfd0f6c1a
Pascal Gillet <pascal.gillet@stack-labs.com>
2021-09-15 14:03:20 +0200

    Use quoting to prevent variable expansion

--- 01-High-performance-modern-Java/dm.sh
+++ 01-High-performance-modern-Java/dm.sh
@@ -5,16 +5,16 @@
 clear
 
  # Generate large text file
-pe "make large.txt"
+pe 'make large.txt'
 
 # Compile
-pe "$GRAALVM_EE_HOME/bin/javac TopTen.java"
+pe '$GRAALVM_EE_HOME/bin/javac TopTen.java'
 
 # Run with GraalVM EE
-pe "time $GRAALVM_EE_HOME/bin/java TopTen large.txt"
+pe 'time $GRAALVM_EE_HOME/bin/java TopTen large.txt'
 
 # Run with GraalVM without the GraalVM JIT compiler
-# pe "time $GRAALVM_EE_HOME/bin/java -XX:-UseJVMCICompiler TopTen large.txt"
+# pe 'time $GRAALVM_EE_HOME/bin/java -XX:-UseJVMCICompiler TopTen large.txt'
 
 # Run with standard HotSpot JVM
-pe "time $JAVA_HOME/bin/java TopTen large.txt"
+pe 'time $JAVA_HOME/bin/java TopTen large.txt'
