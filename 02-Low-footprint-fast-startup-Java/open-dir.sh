c7e2dfb191c5a219481cc532ee003fd97cdbd002
Pascal Gillet <pascal.gillet@stack-labs.com>
2021-09-15 11:08:42 +0200

    Add 02-Low-footprint-fast-startup-Java

---
+++ 02-Low-footprint-fast-startup-Java/open-dir.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+code . TopTen.java Makefile run.sh
---
+++ 02-Low-footprint-fast-startup-Java/run.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+. ../utils/bash-env.sh
+
+echo "Generate small text file"
+make small.txt
+
+stat -c "%s %n" small.txt
+
+echo "Compile"
+$GRAALVM_CE_HOME/bin/javac TopTen.java
+
+echo "Run with GraalVM CE"
+/usr/bin/time -v $GRAALVM_CE_HOME/bin/java TopTen small.txt
+
+echo "Build native image"
+$GRAALVM_CE_HOME/bin/native-image --no-server --no-fallback TopTen
+
+stat -c "%s %n" topten
+
+echo "Inspect the shared libraries used by the topten native executable \U1F9D0"
+ldd topten
+
+echo "Run the executable"
+/usr/bin/time -v ./topten small.txt
+
+# echo "Again, but with a large text input file"
+# make large.txt
+# /usr/bin/time -v ./topten large.txt
