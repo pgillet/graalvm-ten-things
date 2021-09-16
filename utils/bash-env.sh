9e41eefc39c7c5835c6ee07f8a3edfb3387cdbba
Pascal Gillet <pascal.gillet@stack-labs.com>
2021-09-16 14:37:50 +0200

    Update bash env

--- utils/bash-env.sh
+++ utils/bash-env.sh
@@ -5,13 +5,13 @@ SCRIPT_DIR="$(dirname ${BASH_SOURCE[0]})"
 set -eu
 
 function echo() {
-    command echo -e "\n\e[3m\e[32m# $1\e[0m"
+    command echo -e "\n\e[32m# $1\e[0m"
 }
 
 function process_command() {
 # Print commands except echo commands
 if [[ $BASH_COMMAND != echo* ]]; then
-    printf "\e[1m%s\e[0m\n" "$BASH_COMMAND"
+    printf "\e[1m> %s\e[0m\n" "$BASH_COMMAND"
     read -p $'\e[90mPress enter to continue\e[0m'
 fi
 }
