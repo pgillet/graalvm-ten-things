4d27a2c37a3324c8c078eb68a9f4ac610df1b69f
Pascal Gillet <pascal.gillet@stack-labs.com>
2021-09-28 11:31:21 +0200

    Show build commands in Spring Native example

--- 04-Spring-Native/run-native.sh
+++ 04-Spring-Native/run-native.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 . ../utils/bash-env.sh
 
+echo "The native image can be built as follows:\n\e[97m\e[1m> mvn -Pnative -DskipTests package"
+
 echo "Run the native application"
 target/rest-service-complete
 
--- 04-Spring-Native/run-standard.sh
+++ 04-Spring-Native/run-standard.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 . ../utils/bash-env.sh
 
+echo "Build"
+JAVA_HOME=${GRAALVM_CE_HOME}
+mvn -DskipTests package
+
 echo "Run the standard Spring Boot application"
 java -jar target/rest-service-complete-0.0.1-SNAPSHOT-exec.jar
 
