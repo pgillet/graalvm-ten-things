479a353c3403aec295cb7437ff8216d761e3a07e
Pascal Gillet <pascal.gillet@stack-labs.com>
2021-09-28 09:57:51 +0200

    Add run.sh to simplelanguage example

---
+++ 09-Simple-Language/run.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+. ../utils/bash-env.sh
+
+echo "Setup"
+$GRAALVM_CE_HOME/bin/gu install native-image
+$GRAALVM_CE_HOME/bin/gu list
+
+echo "Build"
+cd simplelanguage/
+export SL_BUILD_NATIVE=true
+export JAVA_HOME=$GRAALVM_CE_HOME
+mvn package -DskipTests
+
+echo "Run examples"
+cat language/tests/HelloWorld.sl
+
+$GRAALVM_CE_HOME/bin/java \
+    -cp launcher/target/launcher-21.2.0.jar \
+    -Dtruffle.class.path.append=language/target/simplelanguage.jar \
+    com.oracle.truffle.sl.launcher.SLMain language/tests/HelloWorld.sl
+
+more sl
+./sl language/tests/HelloWorld.sl
+
+cat language/tests/SumPrint.sl
+./sl language/tests/SumPrint.sl
+
+cat language/tests/Fibonacci.sl
+./sl language/tests/Fibonacci.sl
+
+echo "Simple Language Component for GraalVM"
+du -h component/sl-component.jar
+$GRAALVM_CE_HOME/bin/gu -L install component/sl-component.jar
+$GRAALVM_CE_HOME/bin/gu list
+
+echo "Simple Language Native Image"
+du -h ./native/slnative
+time ./sl language/tests/HelloWorld.sl
+time ./native/slnative language/tests/HelloWorld.sl
