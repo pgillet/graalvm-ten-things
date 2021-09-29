9a3f7c2414332d7374eceb9e6f24cf187dec8a44
Pascal Gillet <pascal.gillet@stack-labs.com>
2021-09-29 10:11:32 +0200

    Add dirty make.sh

---
+++ make.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+make clean
+cd /home/pascal/Workbench/Projects/graal-demo/graalvm-ten-things/03-CLI-applications-with-Micronaut-and-GraalVM-Native-Image/
+make build
+cd /home/pascal/Workbench/Projects/graal-demo/graalvm-ten-things/04-Spring-Native/
+make build
+cd /home/pascal/Workbench/Projects/graal-demo/graalvm-ten-things/09-Simple-Language/
+make build
